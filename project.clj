6e83a92fbb2909953bef1417af0b935f4243bed0
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-11-16 23:37:53 -0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject garden/garden-color "0.1.0-SNAPSHOT"
+  :description "Utilities for working with color."
+  :url "http://github.com/garden-clojure/garden-color"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies
+  [[org.clojure/clojure "1.6.0"]]
+
+  :profiles
+  {:dev {:dependencies [[org.clojure/test.check "0.6.1"]]}}) 
