3f0a45e9a8e9d3a61e1aec29f6bd211dd426a168
Joel Holdbrooks <cjholdbrooks@gmail.com>
2016-10-24 22:58:34 -0700

    Update user namespace

--- dev/user.clj
+++ dev/user.clj
@@ -1,9 +1,19 @@
 (ns user
   (:require
-   [weasel.repl.websocket]
-   [cemerick.piggieback]))
+   [cemerick.piggieback :as piggieback]
+   [cljs.repl :as repl]
+   [cljs.repl.node :as node]
+   [weasel.repl.websocket]))
 
 (defn ws-repl []
-  (cemerick.piggieback/cljs-repl
-   :repl-env (weasel.repl.websocket/repl-env
-              :ip "0.0.0.0" :port 9123)))
+  (piggieback/cljs-repl
+   (weasel.repl.websocket/repl-env
+    :ip "0.0.0.0" :port 9123)))
+
+(defn node-repl []
+  (piggieback/cljs-repl
+   (node/repl-env)
+   :output-dir "out"
+   :optimizations :none
+   :cache-analysis true                
+   :source-map true))
